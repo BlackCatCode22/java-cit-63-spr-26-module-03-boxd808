a4b836b64724ac6197f29cbc6e516c677c15f7c1
boxd808 <141696819+boxd808@users.noreply.github.com>
2026-02-22 03:06:25 -0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+package mystudent;
+
+public class App {
+    public static void main(String[] args) {
+        mystudent.Student myStudent = new mystudent.Student();
+        myStudent.name = "John Doe";
+        myStudent.gpa = 3.8;
+        myStudent.sayHello();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+package mystudent;
+
+public class Student {
+    public String name;
+    public double gpa;
+
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name + " and my GPA is " + gpa);
+    }
+}
